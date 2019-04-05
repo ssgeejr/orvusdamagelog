c28d67cfaa74b903937ee5f1961f21f4118e58a2
ioexcept <ioexcept@gmail.com>
2019-04-04 21:44:42 -0500

    updates

--- combatlogengine.py
+++ combatlogengine.py
@@ -14,61 +14,56 @@ action_list = ["HEAL","COMBAT"]
 
 def main(args=None):
     openConnection()
-#    preload()
-#    testtwo()
-#    testthree()
+    preload()
+    results()
+    closeConnection()
+
+def results():
+    global dbcon, cur
+    # cur.execute("select time,action,amount,source,target from orbus where target = 'Rushed Stafrusher(18320)' and action = 'COMBAT'")
+    # cur.execute("select sum(amount),source,target from orbus where target = 'Rushed Stafrusher(18320)' and action = 'COMBAT' group by source");
+#    cur.execute("select sum(amount) as totaldamage,source,target from orbus where action = 'COMBAT' group by source order by totaldamage desc");
+    cur.execute("select sum(amount) as totaldamage,source,target from orbus where action = 'HEAL' group by source, target order by totaldamage desc");
+
+    # zz = 0
+    for row in cur:
+        print(row)
+        # zz += 1
+        # if zz > 10: exit(0)
 
 def openConnection():
     global dbcon, cur
     try:
         dbcon = sqlite3.connect(":memory:")
+        #ensure we auto-commit
         dbcon.isolation_level = None
         print(dbcon)
         cur = dbcon.cursor()
-        cur.execute("create table orbus(ttime,)")
-        cur.execute("insert into test(i) values (99)")
-        cur.execute("insert into test(i) values (100)")
-        cur.execute("select sum(i) from test")
-        print(cur.fetchone()[0])
+        cur.execute("create table orbus(time,action,amount,source,target)")
+#        cur.execute("insert into orbus(time,action,amount,source,target) values('20:52:38:001','HEAL',1000,'Stewdog','Bral')")
+#        cur.execute("insert into orbus(time,action,amount,source,target) values('20:52:38:110','COMBAT',75,'Stewdog','Bral')")
+#        cur.execute("select time,action,amount,source,target from orbus")
+
+#        for row in cur:
+#            print(row)
+
     except Exception as dbe:
         print("************ SOMETHING WENT TRAGICALLY WRONG (EXCEPTION) ****************")
         print(str(dbe))
         print("****************************************")
-    finally:
-        dbcon.close()
+#    finally:
+ #       dbcon.close()
 
-def mysqlConnection():
-    global db
-    try:
-        db = mysql.connector.connect(user='combatengine', password='orbusdb',
-                                  host='127.0.0.1',
-                                  database='ORBUSDB')
-        print(db)
-    except Exception as dbe:
-        print("************ SOMETHING WENT TRAGICALLY WRONG (EXCEPTION) ****************")
-        print(str(dbe))
-        print("****************************************")
-    finally:
-        db.close()
-
-def testthree():
-    print(".....TEST_TRE.....")
-#    import tailer
-    for line in tailer.follow(open('combat.log')):
-        print(line)
-
-
-
-
-
+def closeConnection():
+    dbcon.close()
 
 def parseline(line):
-    global action_list
+    global action_list, cur, dbcon
     if "You are" in line:
         return
 #    print(line)
     if " [Combat] " in line:
-        print(line)
+        # print(line)
 #start at 22
 #        damagelog = line.
 #        playerlog = line.split()
@@ -76,30 +71,33 @@ def parseline(line):
             i = 0
             time = line[0:12]
             tookloc = line.index(" took")
-            print("location %s" % tookloc)
+            # print("location %s" % tookloc)
             target = line[22:tookloc]
-            print("target [%s]" % target)
-            print(line[tookloc])
+            # print("target [%s]" % target)
+            # print(line[tookloc])
             playerlog = line[tookloc:].split()
-            print(playerlog)
+            # print(playerlog)
             dmg_heelz = float( playerlog[1])
             dmg_heelz_type = 1
             if dmg_heelz < 0:
-                print("***HEALING***")
+                # print("***HEALING***")
                 dmg_heelz_type = 0
-            else:
-                print("***HEALING***")
-            TIME ACTION ACTION_KEY AMOUNT SOURCE TARGET CRITICAL ACTUAL
-            print(".............")
-            print("TIME: %s" % time)
-            print("ACTION: %s" % action_list[dmg_heelz_type])
-            print("ACTION_KEY: %s" % dmg_heelz_type)
-            print("AMOUNT: %s" % dmg_heelz)
-            print("SOURCE: %s" % playerlog[4])
-            print("TARGET: %s" % target)
-            print("CRITICAL: %s" % "NOT_IMPLEMENTED")
-            print("ACTUAL: %s" % line)
-            print(".............")
+                dmg_heelz = dmg_heelz * -1
+            # else:
+                # print("***HEALING***")
+#            TIME ACTION ACTION_KEY AMOUNT SOURCE TARGET CRITICAL ACTUAL
+#             print(".............")
+            # print("TIME: %s" % time)
+            # print("ACTION: %s" % action_list[dmg_heelz_type])
+            # print("ACTION_KEY: %s" % dmg_heelz_type)
+            # print("AMOUNT: %s" % dmg_heelz)
+            # print("SOURCE: %s" % playerlog[4])
+            # print("TARGET: %s" % target)
+            # print("CRITICAL: %s" % "NOT_IMPLEMENTED")
+            # print("ACTUAL: %s" % line)
+            # print(".............")
+            cur.execute("insert into orbus(time,action,amount,source,target) values(?,?,?,?,?)" , (time,action_list[dmg_heelz_type],dmg_heelz,playerlog[4],target))
+
 
 #            print(" >> target[%s] damage[%s] player[%s]" % (target,dmg_heelz,playerlog[3],))
 #            while i < len(playerlog):
@@ -111,6 +109,8 @@ def parseline(line):
             print(line)
             print(str(e))
             #18:13:02:562 [Combat] You are now in combat.
+            # exit(-1)
+            return
             print("****************************************")
 
 
@@ -133,32 +133,8 @@ def parseline(line):
 #        print(" >> target[%s] damage[%s] player[%s] [%s]" % (parsedlog[2],parsedlog[4],parsedlog[7],line))
 
 
-def XparselineX(line):
-    global ford_dmg, ford_cnt, chevy_dmg, chevy_cnt, cntr
-#    print(line)
-    brick = line.split('^')
-    if brick[0] == 'Ford':
-        ford_dmg += float(brick[1])
-        ford_cnt = 1
-
-    if brick[0] == 'Chevrolet':
-        chevy_dmg += float(brick[1])
-        chevy_cnt += 1
-
-def printdmg():
-    global ford_dmg, ford_cnt, chevy_dmg, chevy_cnt, cntr
-    print("Ford Dmg: [%s]" % dps(ford_dmg, ford_cnt))
-    print("Chevy Dmg: [%s]" % dps(chevy_dmg, chevy_cnt))
-    print("..................................")
-    cntr = 0
-    ford_cnt = 0
-    chevy_cnt = 0
-    chevy_dmg = 0
-    ford_dmg = 0
-
-
 def preload():
-    print(".....TEST_ONE.....")
+    print(".....preload.....")
     filepath = 'combat.out'
     global cntr
     with open(filepath) as fp:
@@ -176,29 +152,6 @@ def preload():
 #    print("Chevy Dmg: [%s]" % dps(chevy_dmg, chevy_cnt))
 #    print("..................................")
 #    print("..................................")
-
-
-def dps(x,y):
-    try:
-        return round(x/y,2)
-    except ZeroDivisionError:
-        return 0.00
-
-def testtwo():
-    print(".....TEST_TWO.....")
-    f = open('combat.log')
-    p = 0
-    while True:
-     #   print('seeking ...')
-        f.seek(p)
-        latest_data = f.read()
-        p = f.tell()
-        if latest_data:
-            print
-            print(latest_data)
-            print
-#            str(p).center(10).center(80, '=')
-
 
 if __name__ == "__main__":
     main()
